6ee5c54e6b9de19ea691296815f6caeab53caf63
AliParky <aparkinson670@gmail.com>
2026-09-26 00:03:25 +0800

    Add board parameter to line_expected_value function

--- cactpot.py
+++ cactpot.py
@@ -54,7 +54,7 @@ def parse_board():
         board.append(value)
     return board
 
-def line_expected_value(line_name):
+def line_expected_value(board, line_name):
     line = LINES[line_name]
     return
 
